45d3cd533d452562ab3ccf043bb9159b5cf667ff
micciu1 <gnomiok123@gmail.com>
2024-09-16 12:06:45 +0200

    updated context.h

--- src/layer2/context.h
+++ src/layer2/context.h
@@ -36,81 +36,115 @@ typedef enum {
 
 typedef uint8_t qfi_t;
 
-typedef struct {
-    qfi_t qfi;
+typedef struct ogs_ran_header_s {
     uint8_t *data;
-    uint16_t size;
+    uint8_t size;
+} ogs_ran_header_t;
+
+typedef struct ogs_ran_pdu_header_s {
+    uint8_t *data;
+    uint8_t size;
+    uint8_t sequence_number;
+} ogs_ran_pdu_header_t;
+
+typedef struct ogs_ran_security_header_s {
+    uint8_t extended_protocol_discriminator;
+    uint8_t security_header_type;
+    uint32_t message_authentication_code;
+    uint8_t sequence_number;
+} ogs_ran_security_header_t;
+
+typedef struct {
+    ogs_ran_header_t h;
+    union{
+        qfi_t qfi;
+    };
 } sdap_entity_t;
 
 typedef struct {
-    uint8_t harq_process_id;
-    bool is_active;
-    uint8_t retransmission_count;
+    ogs_ran_header_t h;
+    union{
+        uint8_t harq_process_id;
+        bool is_active;
+        uint8_t retransmission_count;
+    };
 } harq_entity_t;
 
 typedef struct {
-    uint32_t prb_allocation;    /* Physical Resource Block allocation */
-    uint32_t modulation_scheme; /* Modulation scheme (e.g., QPSK, 16QAM) */
-    uint32_t frequency;         /* Carrier frequency */
-    uint8_t tx_power;           /* Transmission power */
-    bool is_signal_shaped;
+    ogs_ran_header_t h;
+    union{
+        uint32_t prb_allocation;    /* Physical Resource Block allocation */
+        uint32_t modulation_scheme; /* Modulation scheme (e.g., QPSK, 16QAM) */
+        uint32_t frequency;         /* Carrier frequency */
+        uint8_t tx_power;           /* Transmission power */
+        bool is_signal_shaped;
+    };
 } phy_entity_t;
 
 typedef struct {
-    uint16_t seq_nr;
-    bool is_control_plane;
-    uint8_t *data;
-    uint32_t size;
+    ogs_ran_header_t h;
+    union 
+    {
+        uint8_t seq_nr;
+        bool is_control_plane;
+    };
 } pdcp_sdu_t;
 
 typedef struct {
-    uint32_t seq_nr;
-    uint8_t *data;
-    uint32_t size;
+    ogs_ran_header_t h;
+    {
+        uint8_t seq_nr;
+    };
 } rlc_sdu_t;
 
 typedef struct {
-    logical_channel_t logical_channel;
-    uint8_t *data;
-    uint16_t size;
+    ogs_ran_header_t h;
+    union {
+        logical_channel_t logical_channel;
+    };
 } mac_sdu_t;
 
 typedef struct {
-    uint32_t seq_nr;
-    bool rohc_enabled;                
-    bool ciphering_enabled;
-    bool integrity_protection_enabled;
-    uint8_t *data;
-    uint16_t size;
+    ogs_ran_pdu_header_t h;
+    union{
+        bool rohc_enabled;                
+        bool ciphering_enabled;
+        bool integrity_protection_enabled;
+    };
 } pdcp_pdu_t;
 
 typedef struct {
-    rlc_mode_t mode;
-    uint32_t seq_nr; 
-    bool arq_enabled;                  
-    bool segmentation_enabled;
-    bool reassembly_enabled;
-    uint8_t *data;
-    uint32_t size;
+    ogs_ran_pdu_header_t h;
+    union {
+        rlc_mode_t mode;
+        bool arq_enabled;                  
+        bool segmentation_enabled;
+        bool reassembly_enabled;
+    };
 } rlc_pdu_t;
 
 typedef struct {
-    transport_channel_t transport_channel;
-    uint16_t tb_size;
-    harq_entity_t harq_entity;
-    uint8_t *data;
+    ogs_ran_pdu_header_t h;
+    union {
+        transport_channel_t transport_channel;
+        uint8_t tb_size;
+        harq_entity_t harq_entity;
+    };
 } mac_pdu_t;
 
 typedef struct {
-    direction_t direction;   
-    mac_sdu_t mac_sdu;
-    mac_pdu_t mac_pdu;
-    rlc_sdu_t rlc_sdu;
-    rlc_pdu_t rlc_pdu;
-    pdcp_sdu_t pdcp_sdu;
-    pdcp_pdu_t pdcp_pdu;
-    sdap_entity_t sdap_entity;
-    phy_entity_t phy_entity;
+    ogs_ran_security_header_t h;
+    union {
+        direction_t direction;   
+        mac_sdu_t mac_sdu;
+        mac_pdu_t mac_pdu;
+        rlc_sdu_t rlc_sdu;
+        rlc_pdu_t rlc_pdu;
+        pdcp_sdu_t pdcp_sdu;
+        pdcp_pdu_t pdcp_pdu;
+        sdap_entity_t sdap_entity;
+        phy_entity_t phy_entity;
+    };
 } data_flow_t;
 
 /* Function Prototypes */
