4f0b5975e6da3ae38e82eccde761fba5c22daa93
Klaudiusz Pastor <pastor.klo12@gmail.com>
2024-09-25 20:29:22 +0200

    RACH cleanup/RACH L2 funtions

--- lib/ran/ogs-ran-rach.h
+++ lib/ran/ogs-ran-rach.h
@@ -9,45 +9,22 @@ extern "C" {
 #include <bits/stdint-uintn.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include "ran/context.h"
 
-#define RA_TYPE_2STEP_RA 1
-#define RA_TYPE_4STEP_RA 2
-#define MSG3_DATA_BUFFOR 16
-#define CONTENTION_RESOLUTION_IDENTIFIER 4
+#define UL_GRANT_VALUE 100
+#define TIMING_ADVANCE_VALUE 64
+#define RAPID_VALUE 0x1A
 
-typedef struct {
-    uint8_t preamble_format;    
-    uint16_t time_resources;    
-    uint16_t frequency_resources;
-    uint8_t ra_rnti;           
-} prach_config_t;
+#define RA_RNTI_S_ID 3           
+#define RA_RNTI_T_ID 2           
+#define RA_RNTI_F_ID 1           
+#define RA_RNTI_UL_CARRIER_ID 0  
 
-typedef struct {
-    uint16_t root_sequence_index; 
-    uint16_t cyclic_shift;        
-    uint8_t set_type;             
-    uint8_t preamble_format;
-    uint8_t ra_rnti;
-} rach_preamble_t;
-
-typedef struct {
-    uint8_t msg1_preamble;       
-    uint8_t msg2_rar;            
-    uint8_t msg3_pusch_grant;    
-    bool contention_resolution;  
-} rach_type_1_proc_t;
-
-typedef struct {
-    uint8_t msg_a_preamble;      
-    uint8_t msg_b_rar;           
-    uint8_t fallback_pusch_grant;
-    bool contention_resolution;   
-} rach_type_2_proc_t;
-
-typedef struct {
-    uint8_t ra_type;
-} ue_vars_t;
+#define TEMP_C_RNTI 54321
+#define C_RNTI 32678
+#define MSG3_DATA_BUFFER 16
+#define CRI_LENGTH 4
+#define ACK 1
+#define NACK 0
 
 typedef struct {
     uint8_t s_id;           
@@ -57,62 +34,47 @@ typedef struct {
 } ra_rnti_t;
 
 typedef struct {
-    uint8_t resource_block;
-    uint8_t mcs;
-    uint8_t time_domain_allocation;
-    uint16_t crc;
-} dci_t;
-
-typedef struct {
     uint8_t rapid;
     uint16_t tc_rnti;
     uint16_t ta;
     uint8_t ul_grant;
+    uint16_t ra_rnti;
 } rar_t;
 
 typedef struct {
-    uint8_t contention_resoultion_id[CONTENTION_RESOLUTION_IDENTIFIER];
+    uint8_t msg3_data[MSG3_DATA_BUFFER];  
+    uint16_t c_rnti;                      // Temporary C-RNTI identifier
+} connection_request_t;
+
+typedef struct {
+    uint16_t c_rnti;    // C-RNTI to be assigned
+    uint16_t ta;        
+    uint8_t ul_grant;   
+    uint8_t ack_nack;   
+    uint8_t cri[CRI_LENGTH]; // CRI containing first 32 bits of Msg3
 } contention_resolution_t;
 
 typedef struct {
-    bool is_type_1;              
-    prach_config_t prach_config;  
-    rach_preamble_t preamble;    
-    union {
-        rach_type_1_proc_t type_1_proc;
-        rach_type_2_proc_t type_2_proc;
-    };
-    ue_vars_t ue_vars;
-    dci_t dci;
     rar_t rar;
-    contention_resolution_t cri;
-    uint8_t msg3_data[MSG3_DATA_BUFFOR];
-    uint16_t c_rnti;
+    ra_rnti_t ra_rnti_data;
+    connection_request_t msg3;
+    contention_resolution_t msg4;
+
+    uint16_t assigned_c_rnti;
 } rach_procedure_t;
 
-void initialize_2step_ra_variables(rach_procedure_t *rach_proc); 
-void initialize_4step_ra_variables(rach_procedure_t *rach_proc);
-void initialize_ra_variables(rach_procedure_t *rach_proc);
-void select_random_access_resources_2step(rach_procedure_t *rach_proc);
-void select_random_access_resources_4step(rach_procedure_t *rach_proc);
-void ogs_ran_init_random_access_procedure(rach_procedure_t *rach_proc); 
-void ogs_ran_decode_prach_preamble(rach_procedure_t *rach_proc, uint8_t preamble);
+
 uint16_t ogs_ran_calculate_ra_rnti(ra_rnti_t *ra_rnti);
-void phy_send_preamble(phy_entity_t *phy_entity, uint8_t preamble); 
-void ogs_ran_generate_dci_with_ra_rnti(rach_procedure_t *rach_proc, phy_entity_t *phy_entity);
-void ogs_ran_send_rar(rach_procedure_t *rach_proc, phy_entity_t *phy_entity);
-void ogs_ran_process_msg2(rach_procedure_t *rach_proc, phy_entity_t *phy_entity); 
-size_t ogs_ran_simulate_receive_pusch(rach_procedure_t *rach_proc, uint8_t *buffer, size_t buffer_size); 
-void ogs_ran_receive_msg3(rach_procedure_t *rach_proc);
-bool ogs_ran_verify_crc_with_tc_rnti(uint16_t tc_rnti, uint8_t *msg_data, size_t msg_len);
-void ogs_ran_handle_harq_retransmission(rach_procedure_t *rach_proc);
-void ogs_ran_generate_msg4(rach_procedure_t *rach_proc); 
-void ogs_ran_send_msg4(rach_procedure_t *rach_proc, contention_resolution_t *cri); 
-bool ogs_ran_simulate_dci_reception(rach_procedure_t *rach_proc); 
-void ogs_ran_monitor_pdcch_for_msg4(rach_procedure_t *rach_proc); 
-void ogs_ran_decode_msg4(rach_procedure_t *rach_proc); 
-bool ogs_ran_resolve_contention(rach_procedure_t *rach_proc); 
-void ogs_ran_complete_contention_resolution(rach_procedure_t *rach_proc);
+void ogs_ran_send_ra_rnti(uint16_t ra_rnti); 
+void ogs_ran_handle_prach_preamble(uint8_t preamble); 
+void ogs_ran_transmit_msg2_to_phy(rach_procedure_t *rach_proc);
+void ogs_ran_generate_ra_response(rach_procedure_t *rach_proc); 
+
+size_t ogs_ran_receive_msg3_from_phy(rach_procedure_t *rach_proc);
+void ogs_ran_process_msg3_mac(rach_procedure_t *rach_proc); 
+void ogs_ran_send_msg4(rach_procedure_t *rach_proc); 
+void ogs_ran_handle_harq_retransmission(rach_procedure_t *rach_proc); 
+void ogs_ran_process(rach_procedure_t *rach_proc); 
 
 #ifdef __cplusplus
 }
--- src/ran/rach_procedure.c
+++ src/ran/rach_procedure.c
@@ -1,271 +1,137 @@
-#include "ran/context.h"
 #include "ran/ogs-ran-rach.h"
 #include "ogs-core.h"
 #include <string.h>
 #include "ran/ogs-ran-rrc.h"
 
-void initialize_2step_ra_variables(rach_procedure_t *rach_proc) {
-    ogs_assert(rach_proc != NULL);
-    ogs_info("Initializing variables for 2-step RA");
-    // TODO
-    // Initialization of 2-stepRA logic in 5.1.1a 
-}
-
-void initialize_4step_ra_variables(rach_procedure_t *rach_proc) {
-    ogs_assert(rach_proc != NULL);
-    ogs_info("Initializing variables for 4-step RA");
-    //TODO
-    // Intialization of 4-stepRA logic in 5.1.1a
-}
-
-void initialize_ra_variables(rach_procedure_t *rach_proc) {
-    ogs_assert(rach_proc != NULL);
-
-    if (rach_proc->ue_vars.ra_type == RA_TYPE_2STEP_RA) {
-        initialize_2step_ra_variables(rach_proc);
-    } else if (rach_proc->ue_vars.ra_type == RA_TYPE_4STEP_RA) {
-        initialize_4step_ra_variables(rach_proc);
-    } else {
-        ogs_error("Unknown RA_TYPE: %d", rach_proc->ue_vars.ra_type);
-    }
-}
-
-void select_random_access_resources_2step(rach_procedure_t *rach_proc) {
-    ogs_info("Performing Random Access Resource selection for 2-step RA");
-    // Logic to select resources for 2-step RA according to clause 5.1.2a
-}
-
-void select_random_access_resources_4step(rach_procedure_t *rach_proc) {
-    ogs_info("Performing Random Access Resource selection for 4-step RA");
-    // Logic to select resources for 4-step RA according to clause 5.1.2
-}
-
-void ogs_ran_init_random_access_procedure(rach_procedure_t *rach_proc) {
-    ogs_assert(rach_proc != NULL);
-    // Logic to initialize RA procedure according to clause 5.1.1
-    // Initialize variables specific to the selected RA type
-    initialize_ra_variables(rach_proc);
-    if (rach_proc->ue_vars.ra_type == RA_TYPE_2STEP_RA) {
-        select_random_access_resources_2step(rach_proc);
-    } else {
-        select_random_access_resources_4step(rach_proc);
-    }
-}
-
-/* Msg1 Random Access Preamble Transmission */
-
-void phy_send_preamble(phy_entity_t *phy_entity, uint8_t preamble) {
-    ogs_info("Physical layer (ID: %d) processing PRACH preamble 0x%02X", phy_entity->id, preamble);
-}
-
-void ogs_ran_decode_prach_preamble(rach_procedure_t *rach_proc, uint8_t preamble) {
-    ogs_info("Decoding PRACH Preamble: 0x%02X", preamble);
-
-    // Simulating PRACH
-    if (preamble == 0x1A || preamble == 0x1B || preamble == 0x1C) {
-        ogs_info("PRACH preamble 0x%02X received and decoded successfully.", preamble);
-        rach_proc->preamble.preamble_format = preamble;
-
-        // Simulationg of sending preamble to physical layer 
-        phy_entity_t phy_entity;
-        phy_entity.id = rand() % 1000;  
-        ogs_info("Sending PRACH preamble 0x%02X to physical layer entity with ID: %d", preamble, phy_entity.id);
-        phy_send_preamble(&phy_entity, preamble);
-
-        // uint8_t time_resource = rand() % 15; 
-        // uint8_t freq_resource = rand() % 10; 
-        // ogs_info("Assigned Time Resource: %d, Frequency Resource: %d for PRACH transmission.", time_resource, freq_resource);
-    } else {
-        ogs_error("Unknown PRACH preamble received.");
-    }
-}
-
 uint16_t ogs_ran_calculate_ra_rnti(ra_rnti_t *ra_rnti_data) {
-    uint16_t ra_rnti_value = 1 + ra_rnti_data->s_id + 14 * ra_rnti_data->t_id + 14 * 80 * ra_rnti_data->f_id + ra_rnti_data->ul_carrier_id;
-    ogs_info("Calculated RA-RNTI: %d", ra_rnti_value);
+    ogs_assert(ra_rnti_data != NULL);
+    uint16_t ra_rnti_value = 1 + ra_rnti_data->s_id +
+                             14 * ra_rnti_data->t_id +
+                             14 * 80 * ra_rnti_data->f_id +
+                             14 * 80 * 8 * ra_rnti_data->ul_carrier_id;
+    
+    printf("Calculated RA-RNTI: %d\n", ra_rnti_value);
     return ra_rnti_value;
 }
 
-/* Msg2 Random Access Response Reception */
+void ogs_ran_send_ra_rnti(uint16_t ra_rnti) {
+    printf("RA-RNTI %d encoded and sent on PDCCH.\n", ra_rnti);
+}
 
-// void transmit_dci_to_phy_layer(phy_entity_t *phy_entity, dci_t *dci) {
-//     ogs_info("Transmitting DCI to Physical Layer (ID: %d), Resource Block: %d, MCS: %d",
-//              phy_entity->id, dci->resource_block, dci->mcs);
-// }
+void ogs_ran_handle_prach_preamble(uint8_t preamble) {
+    printf("Received PRACH preamble: 0x%02X\n", preamble);
 
-// void transmit_rar_to_phy_layer(phy_entity_t *phy_entity, rar_t *rar) {
-//     ogs_info("Transmitting RAR to Physical Layer (ID: %d), RAPID: %d, TC-RNTI: %d, TA: %d, UL Grant: %d",
-//              phy_entity->id, rar->rapid, rar->tc_rnti, rar->ta, rar->ul_grant);
-// }
-
-void ogs_ran_generate_dci_with_ra_rnti(rach_procedure_t *rach_proc, phy_entity_t *phy_entity) {
-    ogs_info("Generating DCI with RA-RNTI: %d", rach_proc->preamble.ra_rnti);
-
-    uint8_t resource_block = rand() % 100;  
-    uint8_t modulation_and_coding_scheme = rand() % 28;  
-    uint8_t time_domain_allocation = rand() % 10;  
-
-    // DCI (Format 1_0) structure
-    rach_proc->dci.resource_block = resource_block;
-    rach_proc->dci.mcs = modulation_and_coding_scheme;
-    rach_proc->dci.time_domain_allocation = time_domain_allocation;
+    ra_rnti_t ra_rnti_data = {
+        .s_id = RA_RNTI_S_ID,  
+        .t_id = RA_RNTI_T_ID,
+        .f_id = RA_RNTI_F_ID,
+        .ul_carrier_id = RA_RNTI_UL_CARRIER_ID  
+    };
+    uint16_t ra_rnti = ogs_ran_calculate_ra_rnti(&ra_rnti_data);
     
-    ogs_info("DCI Format 1_0 generated: Resource Block: %d, MCS: %d, Time Domain Allocation: %d",
-             resource_block, modulation_and_coding_scheme, time_domain_allocation);
-    ogs_info("DCI with RA-RNTI: %d transmitted on PDCCH with Resource Block: %d and MCS: %d",
-             rach_proc->preamble.ra_rnti, resource_block, modulation_and_coding_scheme);
-
-//     transmit_dci_to_phy_layer(phy_entity, &rach_proc->dci);
+    ogs_ran_send_ra_rnti(ra_rnti);
 }
 
-void ogs_ran_send_rar(rach_procedure_t *rach_proc, phy_entity_t *phy_entity) {
-    ogs_info("Sending RAR on PDSCH for RA-RNTI: %d", rach_proc->preamble.ra_rnti);
-    ogs_info("Transmitting RAR on Physical Layer (ID: %d) with Resource Block: %d and MCS: %d",
-             phy_entity->id, rach_proc->dci.resource_block, rach_proc->dci.mcs);
-
-//     transmit_rar_to_phy_layer(phy_entity, &rach_proc->rar);
+void ogs_ran_transmit_msg2_to_phy(rach_procedure_t *rach_proc) {
+    ogs_info("Transmitting Msg2 (RAR) to PHY on PDSCH...");
+    ogs_info("Msg2 (RAR) content:");
+    ogs_info("RAPID: %d", rach_proc->rar.rapid);
+    ogs_info("RA-RNTI: %d", rach_proc->rar.ra_rnti);
+    ogs_info("Timing Advance (TA): %d", rach_proc->rar.ta);
+    ogs_info("Uplink Grant: %d", rach_proc->rar.ul_grant);
 }
 
-void ogs_ran_process_msg2(rach_procedure_t *rach_proc, phy_entity_t *phy_entity) {
-    ogs_info("Processing Msg2 for UE with RA-RNTI: %d", rach_proc->preamble.ra_rnti);
+// Function to generate Msg2 (RAR) based on RA-RNTI and network parameters
+void ogs_ran_generate_ra_response(rach_procedure_t *rach_proc) {
+    ogs_assert(rach_proc != NULL);
 
-    ogs_ran_generate_dci_with_ra_rnti(rach_proc, phy_entity);
-    uint8_t rapid = rand() % 64;  
-    uint16_t t_crnti = rand() % 65536;  
-    uint16_t ta = rand() % 128;  
-    uint8_t ul_grant = rand() % 256;  
-    ogs_ran_send_rar(rach_proc, phy_entity);
+    ogs_ran_handle_prach_preamble(RAPID_VALUE);  
+    uint16_t ra_rnti = ogs_ran_calculate_ra_rnti(&rach_proc->ra_rnti_data);
+    rach_proc->rar.ra_rnti = ra_rnti;
+    rach_proc->rar.ul_grant = UL_GRANT_VALUE;
+    rach_proc->rar.ta = TIMING_ADVANCE_VALUE;
+    rach_proc->rar.rapid = RAPID_VALUE;
+    ogs_ran_transmit_msg2_to_phy(rach_proc);
 }
 
-/* Msg3 Connection Request Transmission */
+// Function to simulate Msg3 reception from PHY layer (received over PUSCH)
+size_t ogs_ran_receive_msg3_from_phy(rach_procedure_t *rach_proc) {
+    ogs_info("Receiving Msg3 from UE on PUSCH...");
 
-void ogs_ran_receive_msg3(rach_procedure_t *rach_proc) {
-    ogs_info("Receiving Msg3 on PUSCH for UE with TC-RNTI: %d", rach_proc->rar.tc_rnti);
+    // Simulate Msg3 reception with global Temporary C-RNTI
+    uint16_t temp_c_rnti = TEMP_C_RNTI;
+    rach_proc->msg3.c_rnti = temp_c_rnti;  
 
-    uint8_t msg3_data[MSG3_DATA_BUFFOR]; 
-    // Receiving data from PUSCH 
-    size_t msg3_len = ogs_ran_simulate_receive_pusch(rach_proc, msg3_data, sizeof(msg3_data));
-
-    if (msg3_len > 0) {
-        bool crc_valid = ogs_ran_verify_crc_with_tc_rnti(rach_proc->rar.tc_rnti, msg3_data, msg3_len);
-
-        if (crc_valid) {
-            ogs_info("Msg3 successfully received and verified.");
-            rrc_message_type_t msg_type = RRC_SETUP_REQUEST;
-            rrc_dispatch_message(msg_type, msg3_data, msg3_len);
-
-        } else {
-            ogs_error("Msg3 CRC verification failed. Requesting HARQ retransmission.");
-            ogs_ran_handle_harq_retransmission(rach_proc);
-        }
+    size_t msg3_len = CRI_LENGTH;  
+    if (msg3_len <= MSG3_DATA_BUFFER) {
+        memset(rach_proc->msg3.msg3_data, 0xAB, msg3_len);  
+        ogs_info("Received Msg3 with Temporary C-RNTI: %u", temp_c_rnti);
     } else {
-        ogs_error("Failed to receive Msg3 on PUSCH.");
+        ogs_error("Msg3 buffer overflow.");
+        return 0;
+    }
+
+    return msg3_len;
+}
+
+// Function to process Msg3 at the MAC layer in gNB
+void ogs_ran_process_msg3_mac(rach_procedure_t *rach_proc) {
+    ogs_assert(rach_proc != NULL);
+
+    // Check if Msg3 contains Temporary C-RNTI (no RRC message, just MAC CE)
+    if (rach_proc->msg3.c_rnti == TEMP_C_RNTI) {
+        ogs_info("Processing Msg3 for UE with Temporary C-RNTI: %u", rach_proc->msg3.c_rnti);
+
+        // Handle contention resolution (by sending MAC CE: CRI)
+        // Copy the first 32 bits of Msg3 to CRI (Contention Resolution Indication)
+        memcpy(rach_proc->msg4.cri, rach_proc->msg3.msg3_data, CRI_LENGTH);
+        ogs_info("Contention Resolution Indication (CRI) created from first 32 bits of Msg3.");
+
+        rach_proc->msg4.ul_grant = UL_GRANT_VALUE;  
+        ogs_info("Allocated uplink resources for UE: %u, UL Grant: %d", rach_proc->msg3.c_rnti, rach_proc->msg4.ul_grant);
+        rach_proc->msg4.ta = TIMING_ADVANCE_VALUE;  
+        ogs_info("Timing Advance for UE: %u, TA: %d", rach_proc->msg3.c_rnti, rach_proc->msg4.ta);
+        rach_proc->assigned_c_rnti = C_RNTI;
+        ogs_info("Assigning new C-RNTI to UE: %u", rach_proc->assigned_c_rnti);
+        rach_proc->msg4.c_rnti = rach_proc->assigned_c_rnti;
+        rach_proc->msg4.ack_nack = ACK;  // Simulated ACK (1 = success)
     }
 }
 
-size_t ogs_ran_simulate_receive_pusch(rach_procedure_t *rach_proc, uint8_t *buffer, size_t buffer_size) {
-    ogs_info("Simulating PUSCH reception for UE with TC-RNTI: %d", rach_proc->rar.tc_rnti);
-
-    rrc_setup_request_t rrc_setup_req;
-    rrc_setup_req.ueIdentity.mmeCode = 0x1234;
-    rrc_setup_req.ueIdentity.mTmsi = 0x56789;
-
-    size_t msg3_len = sizeof(rrc_setup_req);
-    if (msg3_len <= buffer_size) {
-        memcpy(buffer, &rrc_setup_req, msg3_len);
-    }
-
-    return msg3_len; 
-}
-
-bool ogs_ran_verify_crc_with_tc_rnti(uint16_t tc_rnti, uint8_t *msg_data, size_t msg_len) {
-    bool crc_valid = true; 
-    ogs_info("Verifying CRC with TC-RNTI: %d", tc_rnti);
-
-    return crc_valid;
+void ogs_ran_send_msg4(rach_procedure_t *rach_proc) {
+    ogs_assert(rach_proc != NULL);
+    ogs_info("Sending Msg4 to PDSCH: %u", rach_proc->msg4.c_rnti);
+    ogs_info("Msg4 content: UL Grant: %d, Timing Advance: %d, ACK/NACK: %d",
+             rach_proc->msg4.ul_grant, rach_proc->msg4.ta, rach_proc->msg4.ack_nack);
+    ogs_info("CRI in Msg4 (Contention Resolution Indication): %02X%02X%02X%02X",
+             rach_proc->msg4.cri[0], rach_proc->msg4.cri[1], rach_proc->msg4.cri[2], rach_proc->msg4.cri[3]);
 }
 
 void ogs_ran_handle_harq_retransmission(rach_procedure_t *rach_proc) {
-    ogs_info("Requesting HARQ retransmission for UE with TC-RNTI: %d", rach_proc->rar.tc_rnti);
-}
+    ogs_info("Monitoring HARQ feedback for Msg4...");
 
-/* Msg4 Contention Resolution Reception */
-
-void ogs_ran_generate_msg4(rach_procedure_t *rach_proc) {
-    ogs_info("Generating Msg4 for UE with TC-RNTI: %d", rach_proc->rar.tc_rnti);
-
-    // Simulate: Generating Contention Resolution Indication (CRI) message
-    contention_resolution_t cri;
-    memset(&cri, 0, sizeof(cri));
-
-    // Copy the first 32 bits of Msg3 (UE identifier)
-    memcpy(cri.contention_resoultion_id, rach_proc->msg3_data, 4); 
-
-    // Generate Msg4 with MAC CE: CRI
-    ogs_info("Msg4 contains Contention Resolution Indication for UE.");
-    
-    // Send Msg4 to the physical layer
-    ogs_ran_send_msg4(rach_proc, &cri);
-}
-
-void ogs_ran_send_msg4(rach_procedure_t *rach_proc, contention_resolution_t *cri) {
-    ogs_info("Sending Msg4 on PDSCH for UE with TC-RNTI: %d", rach_proc->rar.tc_rnti);
-    ogs_info("Msg4 with Contention Resolution Indication transmitted on PDSCH.");
-}
-
-bool ogs_ran_simulate_dci_reception(rach_procedure_t *rach_proc) {
-    ogs_info("Simulating DCI reception for UE with TC-RNTI: %d", rach_proc->rar.tc_rnti);
-    return true;
-}
-
-void ogs_ran_monitor_pdcch_for_msg4(rach_procedure_t *rach_proc) {
-    ogs_info("Monitoring PDCCH for Msg4 addressed to UE with TC-RNTI: %d", rach_proc->rar.tc_rnti);
-
-    // Simulate: Monitoring PDCCH
-    bool dci_received = ogs_ran_simulate_dci_reception(rach_proc);
-
-    if (dci_received) {
-        ogs_info("DCI received on PDCCH. Decoding Msg4 from PDSCH.");
-        
-        // Decode the Msg4 from PDSCH
-        ogs_ran_decode_msg4(rach_proc);
+    if (rach_proc->msg4.ack_nack == ACK) {
+        ogs_info("Msg4 successfully received by UE. No retransmission needed.");
     } else {
-        ogs_error("Failed to receive DCI for Msg4. Retrying or aborting.");
+        ogs_error("Msg4 failed. Initiating retransmission...");
+        ogs_ran_send_msg4(rach_proc);
     }
 }
 
-void ogs_ran_decode_msg4(rach_procedure_t *rach_proc) {
-    ogs_info("Decoding Msg4 for UE with TC-RNTI: %d", rach_proc->rar.tc_rnti);
+// Function to decode Msg3 and process Msg4 at the MAC layer
+void ogs_ran_process(rach_procedure_t *rach_proc) {
+    ogs_assert(rach_proc != NULL);
 
-    // Simulate decoding Msg4
-    bool contention_resolved = ogs_ran_resolve_contention(rach_proc);
-
-    if (contention_resolved) {
-        // Promote TC-RNTI to C-RNTI
-        rach_proc->c_rnti = rach_proc->rar.tc_rnti;
-        ogs_info("Contention resolved. Promoting TC-RNTI to C-RNTI: %d", rach_proc->c_rnti);
-
-        // UE transitions to RRC_CONNECTED mode
-        ogs_ran_complete_contention_resolution(rach_proc);
-    } else {
-        ogs_error("Contention resolution failed. Aborting.");
+    // Receive Msg3 from the PHY layer (via PUSCH)
+    size_t msg3_len = ogs_ran_receive_msg3_from_phy(rach_proc);
+    if (msg3_len == 0) {
+        ogs_error("Failed to receive Msg3.");
+        return;
     }
+    // Process Msg3 at the MAC layer (with contention resolution)
+    ogs_ran_process_msg3_mac(rach_proc);
+    // Create and send Msg4 (with CRI)
+    ogs_ran_send_msg4(rach_proc);
+    // Handle HARQ retransmission based on feedback
+    ogs_ran_handle_harq_retransmission(rach_proc);
 }
-
-bool ogs_ran_resolve_contention(rach_procedure_t *rach_proc) {
-    // Simulate: Checking if Msg4 contains the correct UE identifier
-    ogs_info("Resolving contention for UE with TC-RNTI: %d", rach_proc->rar.tc_rnti);
-
-    // Check if the first 32 bits of Msg4 (CRI) match Msg3
-    if (memcmp(rach_proc->msg3_data, rach_proc->cri.contention_resoultion_id, 4) == 0) {
-        return true; 
-    }
-
-    return false; 
-}
-
-void ogs_ran_complete_contention_resolution(rach_procedure_t *rach_proc) {
-    ogs_info("Contention resolution completed. UE with C-RNTI: %d is now in RRC_CONNECTED mode.", rach_proc->c_rnti);
-}
-
-
