53801ca8fe53324886c2e765858b958e68f36bea
Krzysztof Kus <165039473+KrzysztofKus@users.noreply.github.com>
2024-09-27 06:00:44 +0200

    Update registration_request.h
    Mała poprawka wewnątrz struktury.

--- src/ran/registration_request.h
+++ src/ran/registration_request.h
@@ -21,7 +21,7 @@ void run_NGAP_Initial_UE_Message(nas_registration_request_t*, uint8_t, uint32_t)
 typedef struct AMF_temporary_t{
     nas_registration_request_t AMF_registrationRequest;
     uint8_t AMF_establishmentCause;
-        uint32_t AMF_establishmentCause;
+        uint32_t AMF_RAN_UE_NGAP_ID;
 }AMF_temporary_s;
 
 AMF_temporary_s AMF_temporary;
